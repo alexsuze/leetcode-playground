d7ed3ddf6d4d912211400d6f4539836634196953
AlexSu <AlexSu@crm.com.tw>
2026-07-03 18:05:05 +0800

    Leetcode: Roman tp Integer - result

--- EasyLevel/src/RomanToInteger/RomanTransfer.java
+++ EasyLevel/src/RomanToInteger/RomanTransfer.java
@@ -1,10 +1,25 @@
 package RomanToInteger;
 
+import java.util.Arrays;
+
 public class RomanTransfer {
     public int romanToInt(String s) {
         String[] roman = {"I","V","X","L","C","D","M"};
         String[] num = {"1","5","10","50","100","500","1000"};
         String[] str = s.split("");
-        return 0;
+        int[] res = new int[str.length];
+        int sum = 0;
+        System.out.println(Arrays.toString(str));
+        for(int i = 0; i < str.length; i++){
+            int index = Arrays.asList(roman).indexOf(str[i]);
+            res[i] = Integer.parseInt(num[index]);
+            if(i != 0){
+                if(res[i] > res[i-1]){
+                    res[i-1] = -res[i-1];
+                }
+            }
+        }
+        sum = Arrays.stream(res).sum();
+        return sum;
     }
 }
--- EasyLevel/src/RomanToInteger/RunTest.java
+++ EasyLevel/src/RomanToInteger/RunTest.java
@@ -2,6 +2,8 @@ package RomanToInteger;
 
 public class RunTest {
     public static void main(String[] args){
-
+        String s = "MCMXCIV";
+        RomanTransfer res = new RomanTransfer();
+        System.out.println(res.romanToInt(s));
     }
 }
