60ecaca3c2e9557d00614196ab2388c893605220
AlexSu <AlexSu@crm.com.tw>
2026-07-06 10:55:29 +0800

    Leetcode: Two Sum - fix

--- EasyLevel/src/TwoSum/TwoSum.java
+++ EasyLevel/src/TwoSum/TwoSum.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
  */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[0];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -17,7 +16,7 @@ public class TwoSum {
                 }
             }
         }
-        return result;
+        return new int[0];
     }
 
     // 最佳化解法：O(n) - HashMap 一次遍歷，空間換時間
